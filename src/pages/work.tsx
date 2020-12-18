164a3df1d9ca9c7eb73279ae5b94d3575e1766eb
yukihiro.yokogawa@rakus-partners.co.jp <yukihiro.yokogawa@rakus-partners.co.jp>
2020-12-18 15:43:52 +0900

    スライドショーロジックの変更

--- src/pages/work.tsx
+++ src/pages/work.tsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import Work from '~/container/Detail/Work';
-import Slideshow from '~/container/Slideshow';
 
 const work: React.FC = () => {
 	return (
 		<>
 			<div>
-				<Slideshow />
 				<Work />
 			</div>
 		</>
