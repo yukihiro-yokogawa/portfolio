41d679200dbf00a8c09db1d934d7eb24cdaaea12
yukihiro.yokogawa@rakus-partners.co.jp <yukihiro.yokogawa@rakus-partners.co.jp>
2020-12-18 10:59:42 +0900

    Type追加

---
+++ src/container/SideBar.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import SideBar from '~/component/SideBar';
+
+const sideBar: React.FC = () => {
+	return (
+		<>
+			<SideBar />
+		</>
+	);
+};
+
+export default sideBar;
--- src/pages/work.tsx
+++ src/pages/work.tsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import Work from '~/container/Detail/Work';
 import Slideshow from '~/container/Slideshow';
-import SideBar from '~/component/SideBar';
 
 const work: React.FC = () => {
 	return (
 		<>
-			<SideBar />
 			<div>
 				<Slideshow />
 				<Work />
