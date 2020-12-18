5edfcf8cb036474e54c7e2e99d1c03d1e7a3fb18
yukihiro.yokogawa@rakus-partners.co.jp <yukihiro.yokogawa@rakus-partners.co.jp>
2020-12-18 09:44:49 +0900

    制作物ページのサイドバー作成

---
+++ src/component/SideBar.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const sideBar: React.FC = () => {
+	return (
+		<>
+			<nav id="sidebar">
+				<div>1 アンケート</div>
+				<hr />
+				<div>2 ロジカルシンキング</div>
+			</nav>
+		</>
+	);
+};
+
+export default sideBar;
