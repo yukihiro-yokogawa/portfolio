8e5d6ff54734c1a2a45c08dbbe550f896aca6a11
yukihiro.yokogawa@rakus-partners.co.jp <yukihiro.yokogawa@rakus-partners.co.jp>
2021-01-26 10:05:26 +0900

    一部コメント追加

--- src/ducks/ProjectSlice.ts
+++ src/ducks/ProjectSlice.ts
@@ -10,6 +10,7 @@ const projectSlice = createSlice({
 	name: 'projects',
 	initialState,
 	reducers: {
+		// action
 		getProjectRequest: (state, action: PayloadAction<ProjectStates>) => ({
 			...state,
 			projects: action.payload.projects,
@@ -24,6 +25,7 @@ export const { getProjectRequest, getProjectFailure } = projectSlice.actions;
 
 export default projectSlice;
 
+// action実行関数
 export const getProjectsAsync = () => async (dispatch: (arg0: { payload: ProjectStates; type: string }) => void): Promise<void> => {
 	axios.get(`/api/project/get`).then((response) => {
 		const projects: ProjectStates = {
