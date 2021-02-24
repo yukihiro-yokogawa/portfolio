4f083442460be056c94710ce68784870c83d83cb
yukihiro.yokogawa@rakus-partners.co.jp <yukihiro.yokogawa@rakus-partners.co.jp>
2021-02-24 09:44:23 +0900

    skill部分改修

--- src/container/Skill/Skill.tsx
+++ src/container/Skill/Skill.tsx
@@ -1,8 +1,37 @@
+import { useMemo } from 'react';
 import Skill from '~/component/Skill/Skill';
+import { useStoreState } from '~/ducks/selector';
+import _ from 'lodash';
+
 const skill: React.FC = () => {
+	const { skills, techniqueTypes } = useStoreState();
+
+	const mySkillsCreate = useMemo(() => {
+		const mySkills = [];
+		_(techniqueTypes).forEach((techniqueType) => {
+			const mySkill = {};
+			const skillsGroupByTechniqueType = _(skills)
+				.filter((skill) => {
+					return techniqueType.name === skill.technique.techniqueType.name;
+				})
+				.map((skill) => {
+					return {
+						name: `${skill.technique.name}${skill.technique.version === '' ? '' : ` ${skill.technique.version}`}`,
+						level: skill.level,
+					};
+				})
+				.value();
+			mySkill['techniqueType'] = techniqueType.name;
+			mySkill['techniques'] = skillsGroupByTechniqueType;
+			mySkills.push(mySkill);
+		});
+		console.log(mySkills);
+		return mySkills;
+	}, [skills, techniqueTypes]);
+
 	return (
 		<>
-			<Skill />
+			<Skill mySkills={mySkillsCreate} />
 		</>
 	);
 };
