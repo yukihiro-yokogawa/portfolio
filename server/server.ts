502e13c30fcddcd5d94540eab9576f4c40e9b4f2
yukihiro.yokogawa@rakus-partners.co.jp <yukihiro.yokogawa@rakus-partners.co.jp>
2021-02-25 15:02:50 +0900

    ローカル開発環境用サーバー設定

--- server/server.ts
+++ server/server.ts
@@ -6,7 +6,6 @@ const port = process.env.PORT || 3000;
 const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev });
 const handle = app.getRequestHandler();
-console.log(process.env.NODE_ENV);
 
 app.prepare().then(() => {
 	const server = express();
