258d5a1ee3378168228b0768b1243716986afa7b
yukihiro.yokogawa@rakus-partners.co.jp <yukihiro.yokogawa@rakus-partners.co.jp>
2021-02-25 14:38:46 +0900

    vercel サーバー設定変更

--- src/pages/api/index.ts
+++ src/pages/api/index.ts
@@ -6,26 +6,33 @@ const port = process.env.PORT || 3000;
 const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev });
 const handle = app.getRequestHandler();
-console.log(process.env.NODE_ENV);
 const server = express();
 
-app.prepare().then(() => {
-	server.use(
-		'/api',
-		createProxyMiddleware({
-			target: dev ? 'http://localhost:8080' : 'https://yoko-portfolio-backend.herokuapp.com/',
-			changeOrigin: true,
-		}),
-	);
+dev
+	? app.prepare().then(() => {
+			server.use(
+				'/api',
+				createProxyMiddleware({
+					target: 'http://localhost:8080',
+					changeOrigin: true,
+				}),
+			);
 
-	server.all('*', (req, res) => {
-		return handle(req, res);
-	});
+			server.all('*', (req, res) => {
+				return handle(req, res);
+			});
 
-	server.listen(port, (err?: any) => {
-		if (err) throw err;
-		console.log(`> Ready on http://localhost:${port}`);
-	});
-});
+			server.listen(port, (err?: any) => {
+				if (err) throw err;
+				console.log(`> Ready on http://localhost:${port}`);
+			});
+	  })
+	: server.use(
+			'/api',
+			createProxyMiddleware({
+				target: 'https://yoko-portfolio-backend.herokuapp.com/',
+				changeOrigin: true,
+			}),
+	  );
 
 export default server;
