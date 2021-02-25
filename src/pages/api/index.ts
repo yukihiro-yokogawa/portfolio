b85f1a1b372ef45227bacabfcd819f05a4b89a1d
yukihiro.yokogawa@rakus-partners.co.jp <yukihiro.yokogawa@rakus-partners.co.jp>
2021-02-25 14:48:09 +0900

    vercel 設定変更

--- src/pages/api/index.ts
+++ src/pages/api/index.ts
@@ -7,6 +7,7 @@ const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev });
 const handle = app.getRequestHandler();
 const server = express();
+console.log(dev);
 
 dev
 	? app.prepare().then(() => {
