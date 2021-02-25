ecd8b61a24dc03b749bf1bb917cde1e06ffbbfeb
yukihiro.yokogawa@rakus-partners.co.jp <yukihiro.yokogawa@rakus-partners.co.jp>
2021-02-25 14:56:50 +0900

    vercel 設定変更

--- src/pages/api/index.ts
+++ src/pages/api/index.ts
@@ -1,12 +1,39 @@
 import express from 'express';
+import next from 'next';
 import { createProxyMiddleware } from 'http-proxy-middleware';
+
+const port = process.env.PORT || 3000;
+const dev = process.env.NODE_ENV !== 'production';
+const app = next({ dev });
+const handle = app.getRequestHandler();
 const server = express();
-server.use(
-	'/api',
-	createProxyMiddleware({
-		target: 'https://yoko-portfolio-backend.herokuapp.com/',
-		changeOrigin: true,
-	}),
-);
+console.log(dev);
+
+dev
+	? app.prepare().then(() => {
+			server.use(
+				'/api',
+				createProxyMiddleware({
+					target: 'http://localhost:8080',
+					changeOrigin: true,
+				}),
+			);
+
+			server.all('*', (req, res) => {
+				return handle(req, res);
+			});
+
+			server.listen(port, (err?: any) => {
+				if (err) throw err;
+				console.log(`> Ready on http://localhost:${port}`);
+			});
+	  })
+	: server.use(
+			'/api',
+			createProxyMiddleware({
+				target: 'https://yoko-portfolio-backend.herokuapp.com/',
+				changeOrigin: true,
+			}),
+	  );
 
 export default server;
