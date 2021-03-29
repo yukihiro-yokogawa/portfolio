c984511c706c706c0df4366443ecb1ae8b89fd3f
yukihiro.yokogawa@rakus-partners.co.jp <yukihiro.yokogawa@rakus-partners.co.jp>
2021-03-29 17:23:01 +0900

    profile部分デザイン

--- src/component/Profile/Profile.tsx
+++ src/component/Profile/Profile.tsx
@@ -1,7 +1,43 @@
+import { Box, Container } from '@material-ui/core';
 import React from 'react';
 
 const profile: React.FC = () => {
-	return <>profile</>;
+	return (
+		<Container style={{ width: '80%', marginTop: 50 }}>
+			<Box alignItems="center" width={1}>
+				<h3 className="title">自己紹介</h3>
+				<div>
+					自己紹介。自己紹介。自己紹介。自己紹介。自己紹介。自己紹介。自己紹介。自己紹介。自己紹介。自己紹介。自己紹介。自己紹介。自己紹介。自己紹介。自己紹介。自己紹介。自己紹介。自己紹介。自己紹介。自己紹介。自己紹介。自己紹介。自己紹介。自己紹介。自己紹介。自己紹介。自己紹介。自己紹介。自己紹介。自己紹介。自己紹介。自己紹介。自己紹介。自己紹介。自己紹介。自己紹介。自己紹介。自己紹介。自己紹介。自己紹介。自己紹介。自己紹介。自己紹介。自己紹介。自己紹介。自己紹介。自己紹介。自己紹介。自己紹介。自己紹介。自己紹介。自己紹介。自己紹介。自己紹介。自己紹介。自己紹介。自己紹介。自己紹介。自己紹介。自己紹介。自己紹介。自己紹介。自己紹介。自己紹介。自己紹介。自己紹介。自己紹介。自己紹介。自己紹介。自己紹介。自己紹介。自己紹介。自己紹介。自己紹介。自己紹介。自己紹介。自己紹介。自己紹介。自己紹介。
+				</div>
+			</Box>
+			<Box alignItems="center" width={1}>
+				<h3 className="title">直近の学習状況</h3>
+				<ul className="timeline">
+					<li>
+						<p className="timeline-date">Docker</p>
+						<div className="timeline-content">
+							<h3>Dockerの基礎</h3>
+							<p>
+								Dockerの基礎の学習詳細。Dockerの基礎の学習詳細。Dockerの基礎の学習詳細。Dockerの基礎の学習詳細。Dockerの基礎の学習詳細。Dockerの基礎の学習詳細。Dockerの基礎の学習詳細。Dockerの基礎の学習詳細。
+							</p>
+						</div>
+					</li>
+				</ul>
+			</Box>
+			<Box alignItems="center" width={1}>
+				<h3 className="title">経歴</h3>
+				<ul className="timeline">
+					<li>
+						<p className="timeline-date">2007年2月</p>
+						<div className="timeline-content">
+							<h3>経歴のタイトル</h3>
+							<p>経歴の詳細。経歴の詳細。経歴の詳細。経歴の詳細。経歴の詳細。経歴の詳細。</p>
+						</div>
+					</li>
+				</ul>
+			</Box>
+		</Container>
+	);
 };
 
 export default profile;
