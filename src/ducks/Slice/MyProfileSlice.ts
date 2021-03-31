a7af6f8665213a71087faa99109c81d9cb2f5e16
yukihiro.yokogawa@rakus-partners.co.jp <yukihiro.yokogawa@rakus-partners.co.jp>
2021-03-31 14:33:25 +0900

    redux myProfileに関するsliceファイルへのpost通信部分実装

--- src/ducks/Slice/MyProfileSlice.ts
+++ src/ducks/Slice/MyProfileSlice.ts
@@ -1,7 +1,7 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 import axios from 'axios';
 import { MyProfileState } from '~/Type/Profile';
-import { requestFairure } from './NetworkSlice';
+import { requestFairure, requestLoading, requestSuccess } from './NetworkSlice';
 
 export const initialState: Array<MyProfileState> = [
 	{ id: 0, title: '', description: '', date: '', deleted: false, profile: { id: 0, name: '', displayOrder: 0 } },
@@ -12,12 +12,13 @@ const MyProfileSlice = createSlice({
 	initialState,
 	reducers: {
 		getMyProfileRequest: (_state, action: PayloadAction<Array<MyProfileState>>) => [...action.payload],
+		postMyProfileRequest: (_state, action: PayloadAction<Array<MyProfileState>>) => [...action.payload],
 	},
 });
 
 export default MyProfileSlice;
 
-export const { getMyProfileRequest } = MyProfileSlice.actions;
+export const { getMyProfileRequest, postMyProfileRequest } = MyProfileSlice.actions;
 
 export const getMyProfileAsync = () => async (
 	dispatch: (arg0: { payload: Array<MyProfileState>; type: string }) => void,
@@ -26,6 +27,22 @@ export const getMyProfileAsync = () => async (
 		.get(`/api/my_profile/get`, { params: { query: 'GetMyProfile' } })
 		.then((response) => {
 			dispatch(getMyProfileRequest(response.data));
+		})
+		.catch((err) => {
+			console.log(err);
+			dispatch(requestFairure());
+		});
+};
+
+export const postMyProfileAsync = (myProfiles: Array<MyProfileState>) => async (
+	dispatch: (arg0: { payload: Array<MyProfileState>; type: string }) => void,
+): Promise<void> => {
+	dispatch(requestLoading());
+	axios
+		.post('/api/my_profile/post', { params: { data: myProfiles, query: 'PostMyProfile' } })
+		.then(() => {
+			dispatch(postMyProfileRequest(myProfiles));
+			dispatch(requestSuccess());
 		})
 		.catch((err) => {
 			console.log(err);
