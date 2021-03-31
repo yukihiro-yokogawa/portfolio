63a933b9fc7145e451a817337bb8c333d1ee6ef9
yukihiro.yokogawa@rakus-partners.co.jp <yukihiro.yokogawa@rakus-partners.co.jp>
2021-03-31 13:59:42 +0900

    demo用のprofile部分削除

--- src/component/Profile/Profile.tsx
+++ src/component/Profile/Profile.tsx
@@ -34,38 +34,6 @@ const profile: React.FC = () => {
 						) : null}
 					</Box>
 				))}
-				<Box alignItems="center" width={1}>
-					<h3 className="title">自己紹介</h3>
-					<div>
-						自己紹介。自己紹介。自己紹介。自己紹介。自己紹介。自己紹介。自己紹介。自己紹介。自己紹介。自己紹介。自己紹介。自己紹介。自己紹介。自己紹介。自己紹介。自己紹介。自己紹介。自己紹介。自己紹介。自己紹介。自己紹介。自己紹介。自己紹介。自己紹介。自己紹介。自己紹介。自己紹介。自己紹介。自己紹介。自己紹介。自己紹介。自己紹介。自己紹介。自己紹介。自己紹介。自己紹介。自己紹介。自己紹介。自己紹介。自己紹介。自己紹介。自己紹介。自己紹介。自己紹介。自己紹介。自己紹介。自己紹介。自己紹介。自己紹介。自己紹介。自己紹介。自己紹介。自己紹介。自己紹介。自己紹介。自己紹介。自己紹介。自己紹介。自己紹介。自己紹介。自己紹介。自己紹介。自己紹介。自己紹介。自己紹介。自己紹介。自己紹介。自己紹介。自己紹介。自己紹介。自己紹介。自己紹介。自己紹介。自己紹介。自己紹介。自己紹介。自己紹介。自己紹介。自己紹介。
-					</div>
-				</Box>
-				<Box alignItems="center" width={1}>
-					<h3 className="title">直近の学習状況</h3>
-					<ul className="timeline">
-						<li>
-							<p className="timeline-date">Docker</p>
-							<div className="timeline-content">
-								<h3>Dockerの基礎</h3>
-								<p>
-									Dockerの基礎の学習詳細。Dockerの基礎の学習詳細。Dockerの基礎の学習詳細。Dockerの基礎の学習詳細。Dockerの基礎の学習詳細。Dockerの基礎の学習詳細。Dockerの基礎の学習詳細。Dockerの基礎の学習詳細。
-								</p>
-							</div>
-						</li>
-					</ul>
-				</Box>
-				<Box alignItems="center" width={1}>
-					<h3 className="title">経歴</h3>
-					<ul className="timeline">
-						<li>
-							<p className="timeline-date">2007年2月</p>
-							<div className="timeline-content">
-								<h3>経歴のタイトル</h3>
-								<p>経歴の詳細。経歴の詳細。経歴の詳細。経歴の詳細。経歴の詳細。経歴の詳細。</p>
-							</div>
-						</li>
-					</ul>
-				</Box>
 			</Container>
 		</>
 	);
