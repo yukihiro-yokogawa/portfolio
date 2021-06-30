89ce2541516499f2167519eec441f9b42f39e26d
yukihiro-yokogawa <yukihiro-yokogawa@rakus-partners.co.jp>
2021-06-30 09:58:14 +0900

    expressサーバーファイルのコードリファクタリング

--- server/server.ts
+++ server/server.ts
@@ -13,9 +13,17 @@ app.prepare().then(() => {
 
   server.use(express.json());
 
-  server.get("/api/profile/get", (_req, res) => {
+  server.get("/api/:page/:method", (req, res) => {
+    const getUrl = `${url}/api/${req.params.page}/${req.params.method}`;
     axios
-      .get(`${url}/api/profile/get`)
+      .get(
+        getUrl,
+        req.query.id !== null
+          ? {
+              params: { id: req.query.id },
+            }
+          : null
+      )
       .then((response) => {
         res.send(response.data);
       })
@@ -25,155 +33,10 @@ app.prepare().then(() => {
       });
   });
 
-  server.get("/api/my_profile/get", (_req, res) => {
+  server.post("/api/:page/:method", (req, res) => {
+    const postUrl = `${url}/api/${req.params.page}/${req.params.method}`;
     axios
-      .get(`${url}/api/my_profile/get`)
-      .then((response) => {
-        res.send(response.data);
-      })
-      .catch((err) => {
-        console.log(err);
-        res.status(err.statusCode).send(err);
-      });
-  });
-
-  server.post("/api/my_profile/post", (req, res) => {
-    axios
-      .post(`${url}/api/my_profile/post`, req.body.params.data)
-      .then((response) => {
-        res.send(response.data);
-      })
-      .catch((err) => {
-        console.log(err);
-        res.status(err.statusCode).send(err);
-      });
-  });
-
-  server.get("/api/project/get", (_req, res) => {
-    axios
-      .get(`${url}/api/project/get`)
-      .then((response) => {
-        res.send(response.data);
-      })
-      .catch((err) => {
-        console.log(err);
-        res.status(err.statusCode).send(err);
-      });
-  });
-
-  server.get("/api/project/getOne", (req, res) => {
-    axios
-      .get(`${url}/api/project/getOne`, {
-        params: { id: req.query.id },
-      })
-      .then((response) => {
-        res.send(response.data);
-      })
-      .catch((err) => {
-        console.log(err);
-        res.status(err.statusCode).send(err);
-      });
-  });
-
-  server.post("/api/project/post", (req, res) => {
-    axios
-      .post(`${url}/api/project/post`, req.body.params.data)
-      .then((response) => {
-        res.send(response.data);
-      })
-      .catch((err) => {
-        console.log(err);
-        res.status(err.statusCode).send(err);
-      });
-  });
-
-  server.get("/api/technique/get", (_req, res) => {
-    axios
-      .get(`${url}/api/technique/get`)
-      .then((response) => {
-        res.send(response.data);
-      })
-      .catch((err) => {
-        console.log(err);
-        res.status(err.statusCode).send(err);
-      });
-  });
-
-  server.post("/api/technique/post", (req, res) => {
-    axios
-      .post(`${url}/api/technique/post`, req.body.params.data)
-      .then((response) => {
-        res.send(response.data);
-      })
-      .catch((err) => {
-        console.log(err);
-        res.status(err.statusCode).send(err);
-      });
-  });
-
-  server.get("/api/about/get", (_req, res) => {
-    axios
-      .get(`${url}/api/about/get`)
-      .then((response) => {
-        res.send(response.data);
-      })
-      .catch((err) => {
-        console.log(err);
-        res.status(err.statusCode).send(err);
-      });
-  });
-
-  server.post("/api/about/post", (req, res) => {
-    axios
-      .post(`${url}/api/about/post`, req.body.params.data)
-      .then((response) => {
-        res.send(response.data);
-      })
-      .catch((err) => {
-        console.log(err);
-        res.status(err.statusCode).send(err);
-      });
-  });
-
-  server.get("/api/techniqueType/get", (_req, res) => {
-    axios
-      .get(`${url}/api/techniqueType/get`)
-      .then((response) => {
-        res.send(response.data);
-      })
-      .catch((err) => {
-        console.log(err);
-        res.status(err.statusCode).send(err);
-      });
-  });
-
-  server.get("/api/skill/get", (_req, res) => {
-    axios
-      .get(`${url}/api/skill/get`)
-      .then((response) => {
-        res.send(response.data);
-      })
-      .catch((err) => {
-        console.log(err);
-        res.status(err.statusCode).send(err);
-      });
-  });
-
-  server.get("/api/skill/getDeleted", (_req, res) => {
-    axios
-      .get(`${url}/api/skill/getDeleted`)
-      .then((response) => {
-        res.send(response.data);
-      })
-      .catch((err) => {
-        console.log(err);
-        res.status(err.statusCode).send(err);
-      });
-  });
-
-  server.post("/api/skill/post", (req, res) => {
-    axios
-      .post(`${url}/api/skill/post`, req.body.params.data)
+      .post(postUrl, req.body.params.data)
       .then((response) => {
         res.send(response.data);
       })
